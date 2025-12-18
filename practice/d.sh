b17be9e6312330dc6404f467aff86d6fda8df7dd
Vicky <vaikuntaraojagalingi@gmail.com>
2025-12-18 23:20:37 +0530

    added

--- practice/d.sh
+++ practice/d.sh
@@ -2,12 +2,12 @@
 ROOT=$(id -u )
 #checking rootuser or not
 if [ $ROOT -ne 0 ]
-then
-    echo "please run with sudo user"
-    exit 1;
-else
-    echo "installing nginx ..!"
-    dnf install nginx -y  
+    then
+        echo "please run with sudo user"
+        exit 1;
+    else
+        echo "installing nginx ..!"
+        dnf install nginx -y  
 fi
 
 
