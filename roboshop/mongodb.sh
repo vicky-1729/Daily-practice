9d5f7a69a86781c746875262e005d527ce148670
Vicky <vaikuntaraojagalingi@gmail.com>
2025-12-19 19:54:19 +0530

    added

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -17,4 +17,14 @@ if [ $ROOTUSER -ne 0 ];
         echo -e "$G your are sudo user moving further..! $N "
 fi
 
-dnf list installed nginx -y
+VALIDATE(){
+ if [ $1 eq 0 ]
+    then
+    echo -e " $2 is $G successfully $N"
+    else
+    echo -e " $2 is $R failed $N"
+}
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying the mongo repo to /etc/yum.repos.d/mongo.repo"
