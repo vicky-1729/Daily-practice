2e4109748e53bdf5418528ed3323d928ca1fcae5
Vicky <vaikuntaraojagalingi@gmail.com>
2025-12-19 19:48:18 +0530

    added

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -3,10 +3,10 @@
 ROOTUSER=$(id -u)
 # adding colors for best view
 
-R='\e[31m'
-G='\e[32m'
-Y='\e[33m'
-N='\e[0m'
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 #checking the root user
 if [ $ROOTUSER -ne 0 ];
