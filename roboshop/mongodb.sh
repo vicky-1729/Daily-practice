c1b6a80035fde06926a954257b3fc8e921a4df1d
Vicky <vaikuntaraojagalingi@gmail.com>
2025-12-19 19:40:30 +0530

    added

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -3,11 +3,11 @@
 ROOTUSER=$(su -i)
 
 if [ $ROOTUSER -ne 0 ];
-then
-    echo "please run with sudo acess"
-    exit 1;
-else
-    echo "your are sudo user moving further..!"
+    then
+        echo "please run with sudo acess"
+        exit 1;
+    else
+        echo "your are sudo user moving further..!"
 fi
 
 dnf list installed nginx -y
