dbdaa3934f176d8f96daffb76efb33afe59b14ec
Vicky <vaikuntaraojagalingi@gmail.com>
2025-12-19 13:02:10 +0530

    added

--- practice/d.sh
+++ practice/d.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-ROOT=$(id -u )
-#checking rootuser or not
-if [ $ROOT -ne 0 ]
+root_user=$(id -u)
+package=(nginx,python,java,httpd)
+
+
+
+validation(){
+if [ $1 -eq 0]
     then
-        echo "please run with sudo user"
-        exit 1;
-    else
-        echo "installing nginx ..!"
-        dnf install nginx -y  
+    echo "installed succesfully"
+    else 
+    echo "not installed ..!"
+    exit 1;
 fi
-
-
+}
+#checking root user
+if [ ${root_user} -ne 0]
+    then
+    echo "run sudo user"
+    exit 1;
+    else
+    dnf list installed ${package}
+        if [ $? -eq 0]
+            then 
+                echo "already installed ${package}..!"
+            else 
+                echo "installing ${package}..!"
+                dnf install ${package} -y | tee -a ${FILEPATH}
+                validation $? ${package}
+        fi
+fi
